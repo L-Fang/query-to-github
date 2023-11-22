ef8742f99ae46bff26202ca9e4bd413d7dba2216
LF <fang.lorraine@gmail.com>
2023-11-21 22:30:48 -0500

    Update Bank_Customer_View.sql

--- Bank_Customer_View.sql
+++ Bank_Customer_View.sql
@@ -1,7 +1,8 @@
 ---------------------------------------------------------------------------------------
---In this file, some views are created to use this database
+--In this file, three views are created to use this database following with a 'select' query to check if we can get the correct result
 ---------------------------------------------------------------------------------------
---1.Create a view to get all customers with checking account from ON province.
+--1a. Create a view to get all customers with checking account from ON province.
+--1b. A very simple 'select' query to check if the result is correct.
 ---------------------------------------------------------------------------------------
 create view Ch_ON as
 select c.CustomerID,c.CustomerFirstName FirstName,c.CustomerLastName LastName,
@@ -11,14 +12,14 @@ Customer c
 join Account a
 on c.AccountID=a.AccountID and State='ON'
 join AccountType at
-on a.AccountTypeID=at.AccountTypeID and at.AccountTypeID='101' 
--- 101 means checking account
+on a.AccountTypeID=at.AccountTypeID and at.AccountTypeID='101' -- here, 101 means checking account
 ---------------------------------------------------------------------------------------
 --Check the view just created
 select * from Ch_ON
 
 ---------------------------------------------------------------------------------------
---2.Create a view to get all customers with total account balance (including interest rate) greater than 5000.
+--2a. Create a view to get all customers' full name with total account balance (including interest) greater than 10000.
+--2b. A very simple 'select' query to check if the result is correct.
 ---------------------------------------------------------------------------------------
 Create View accountBalance
 AS
@@ -26,20 +27,21 @@ select c.CustomerFirstName || ' ' || c.CustomerMiddleInitial || '. ' || c.Custom
 a.CurrentBalance + a.CurrentBalance * s.InterestRateValue as totalBalance
 from Customer c
 join Account a
-on c.AccountID=a.AccountID
+on c.AccountID = a.AccountID
 join SavingsInterestRates s
 on a.InterestSavingsRateID = s.InterestSavingsRateID
-where (a.CurrentBalance + a.CurrentBalance * s.InterestRateValue) > 5000
+where (a.CurrentBalance + a.CurrentBalance * s.InterestRateValue) > 10000
 
 --Check the view just created
 select * from accountBalance
 
 ---------------------------------------------------------------------------------------
---3. Create a view to get counts of checking and savings accounts by customer.
+--3a. Create a view to get the number of checking and savings accounts for each customer.
+--3b. A very simple 'select' query to check if the result is correct.
 ---------------------------------------------------------------------------------------
 --drop view Count_Ch_Sa
 create view Count_Ch_Sa as
-select C.CustomerID CustomerID,C.CustomerFirstName First_Name,C.CustomerLastName Last_Name, count(A.AccountTypeID) count,AT.AccountTypeDescription
+select C.CustomerID CustomerID,C.CustomerFirstName First_Name,C.CustomerLastName Last_Name, count(A.AccountTypeID) Num_of_Account,AT.AccountTypeDescription
 from
 Customer C
 join Account A
